8247d4399ab7b7c36b5f6d77fb02d381c713ee67
Rohit Raghavan <rohit.raghavan@berkeley.edu>
2017-04-26 23:41:02 -0700

    Added d3 line chart and tableau worksheets

--- js/init.js
+++ js/init.js
@@ -1,8 +1,16 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+(function($) {
+    $(function() {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+$(document).ready(function() {
+    // Enable Select Button (WTF! Why MaterializeCSS??)
+    $('select').material_select();
+    // Enable Carousel
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
+});
